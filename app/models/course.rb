055332ddf430b368228ef6a5278ba13bcdb30131
chausies <mahatmaajaytripathi@gmail.com>
2014-06-23 15:14:00 -0700

    Added validations to Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
   has_many :enrollments, foreign_key: "course_id", dependent: :destroy
   has_many :participants, through: :enrollments
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :school, presence: true, length: { minimum: 2, maximum: 50 }
 end
