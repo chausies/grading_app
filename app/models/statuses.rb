8bc858b5c05ae9364fd4febb56fde4475a3afe29
chausies <mahatmaajaytripathi@gmail.com>
2014-06-23 18:05:02 -0700

    Updated status strings to be more reflective

--- app/models/statuses.rb
+++ app/models/statuses.rb
@@ -1,9 +1,10 @@
 class Statuses
   ###### Enrollment Statuses ######
-  STUDENT = 'a'
-  READER = 'b'
-  INSTRUCTOR = 'c'
-  ADMIN = 'd'
+  STUDENT = '1student'
+  READER = '2reader'
+  TA = '3ta'
+  INSTRUCTOR = '4instructor'
+  ADMIN = '5admin'
 
   # e.g. check if 'user' is a student for course # course_id:
   #     user.enrollments.find_by(course_id: course_id).status?(Statuses::STUDENT)
