e965c8438b77a0cf702aae0e22aaf8ef5ede12f5
chausies <mahatmaajaytripathi@gmail.com>
2014-06-25 14:49:02 -0700

    Gave course controller basic functionality. Made basic course views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,31 @@
 class CoursesController < ApplicationController
+  before_action :instructor_or_more, only: :destroy
+  before_action :TA_or_more, only: [:update, :edit]
+
   def new
+    @course = Course.new
   end
 
   def create
+    @admin_users = User.where(admin: true)
+    @course = Course.new(course_params)
+    if @course.save
+      @admin_users.each { |admin| admin.enroll!(@course) }
+      sign_in @user
+      flash[:success] = "Your course has been successfully created!"
+      redirect_to @course
+    else
+      render 'new'
+    end
   end
 
   def update
+    if @course.update_attributes(course_params)
+      flash[:success] = "Course profile updated"
+      redirect_to @course
+    else
+      render 'edit'
+    end
   end
 
   def edit
@@ -21,4 +41,26 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
   end
+
+  private
+
+    def course_params
+      params.require(:course).permit(:name, :subject, :school)
+    end
+
+    def instructor_or_more
+      status_or_more Statuses::INSTRUCTOR
+    end
+
+    def TA_or_more
+      status_or_more Statuses::TA
+    end
+
+    def status_or_more(status)
+      @course = Course.find(params[:id])
+      has_permission = current_user.enrollments.find_by(course_id: @course.id).status > status
+      unless has_permission
+        redirect_to root_url notice: "You ain't allowed to edit the course ಠ_ಠ"
+      end
+    end
 end
