778274cea497b483e99f2febb8cb4911e3af50d4
chausies <mahatmaajaytripathi@gmail.com>
2014-07-05 03:13:35 -0700

    Gave default sid for enrollment, else can't enforce uniqueness of (sid, email) pairs

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,7 +3,12 @@ class Enrollment < ActiveRecord::Base
   belongs_to :participant, class_name: "User"
   belongs_to :course, class_name: "Course"
 
-  before_save { self.sid = sid.to_s.strip if self.sid }
+  before_save do
+  	self.sid = sid.to_s.strip if self.sid
+  	unless self.id
+  		self.id = ('a'..'z').to_a.shuffle[0..7].join
+  	end
+  end
   validates :participant_id, presence: true
   validates :course_id, presence: true
 
