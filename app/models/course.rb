dead9679e800fbc0c2cd05fe9797619d7774451f
chausies <mahatmaajaytripathi@gmail.com>
2014-07-05 22:31:05 -0700

    Made course has_many assignments

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,7 @@ class Course < ActiveRecord::Base
 
   has_many :enrollments, foreign_key: "course_id", dependent: :destroy
   has_many :participants, through: :enrollments
+  has_many :assignments, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :school, presence: true, length: { minimum: 2, maximum: 50 }
