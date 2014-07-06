978055de19c97a4049732c696416106fd4d4fdb5
chausies <mahatmaajaytripathi@gmail.com>
2014-07-06 06:30:50 -0700

    Added and verified behavior of validations for assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,6 @@
 class Assignment < ActiveRecord::Base
   # Attributes: name, course_id
   belongs_to :course, class_name: "Course"
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :course_id, presence: true
 end
