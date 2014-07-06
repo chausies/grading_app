9ce18b3cdd44fb1904ed7249091cc0b391178cc7
chausies <mahatmaajaytripathi@gmail.com>
2014-07-06 08:46:51 -0700

    Updated description of assignment attributes to include pdf

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ActiveRecord::Base
-  # Attributes: name, course_id
+  # Attributes: name, course_id, pdf
   belongs_to :course, class_name: "Course"
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :course_id, presence: true
