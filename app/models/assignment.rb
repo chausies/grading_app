8d559cdd9b5deb92abc57b86ccae115496001180
chausies <mahatmaajaytripathi@gmail.com>
2014-07-09 15:23:41 -0700

    enrollments and assignments has_many submissions

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,8 @@
 class Assignment < ActiveRecord::Base
   # Attributes: name, course_id, pdf
   belongs_to :course, class_name: "Course"
+  has_many :submissions, dependent: :destroy
+  
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :course_id, presence: true
   mount_uploader :pdf, PdfUploader
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,7 +2,7 @@ class Enrollment < ActiveRecord::Base
   # Attributes: participant_id, course_id, status, sid
   belongs_to :participant, class_name: "User"
   belongs_to :course, class_name: "Course"
-
+  has_many :submissions, dependent: :destroy
   before_save do
   	self.sid = sid.to_s.strip if self.sid
   	unless self.id
