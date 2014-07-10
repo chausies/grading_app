279634725f279ea7fff3334e258a9d95df255880
chausies <mahatmaajaytripathi@gmail.com>
2014-07-10 13:56:51 -0700

    Made submissions ordered in descending created_at order

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,6 @@
 class Submission < ActiveRecord::Base
   # Attributes: assignment_id, course_id, created_at
+  default_scope -> { order('created_at DESC') }
   belongs_to :enrollment, class_name: "Enrollment"
   belongs_to :assignment, class_name: "Assignment"
 
