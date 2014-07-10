30fdeaa4732f0128e8b9fa4ebeab35a1c1a663de
chausies <mahatmaajaytripathi@gmail.com>
2014-07-10 14:15:11 -0700

    filled out submission controller nicely

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,13 +1,45 @@
 class SubmissionController < ApplicationController
+  before_action :set_course
+  before_action :signed_in_user
+  before_action :set_enrollment
+  before_action :set_assignment
+
   def new
+    @submission = @assignment.submissions.build
   end
 
   def create
+    @submission = @assignment.submissions.build(assignment_params)
+
+    if @submission.save
+      flash[:success] = 'Successfully submitted.'
+      redirect_to [@course, @assignment, @submission]
+    else
+      render :new
+    end
   end
 
   def show
+    @submission = @assignment.submissions.find(params[:id])
   end
 
-  def index
-  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_assignment
+      @assignment = @course.assignments.find(params[:assignment_id])
+    end
+
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
+    def set_enrollment
+      @enrollment = current_user.nil? ? false : current_user.enrollments.find_by(course_id: @course.id)
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def assignment_params
+      params[:submission].permit(:pdf)
+    end
+
 end
