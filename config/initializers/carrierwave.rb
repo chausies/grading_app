41814afb7d594eb58c15a840a25fcef63c63e7ed
chausies <mahatmaajaytripathi@gmail.com>
2014-07-12 02:51:07 -0700

    Made s3 info in ENV variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: "AKIAISILFS4Y6TF7YIWA",
-    aws_secret_access_key: "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",
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'us-west-1'
   }
-  config.fog_directory = "magicgrader"
+  config.fog_directory = ENV["AWS_S3_BUCKET"]
 end
