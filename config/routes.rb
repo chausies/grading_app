846ad641dea2ccc37387d77632202062a352c107
chausies <mahatmaajaytripathi@gmail.com>
2014-07-12 08:31:38 -0700

    Fixed small typo in routes file (member instead of members)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :courses do
     resources :assignments do
       resources :submissions, only: [:new, :create, :show]
-      members do
+      member do
         put :begin_grading
       end
     end
