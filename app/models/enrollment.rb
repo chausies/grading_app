e36ef7ff21e862c211489cff5bc6830a76c9378f
chausies <mahatmaajaytripathi@gmail.com>
2014-07-14 17:41:38 -0700

    Updated attributes (no more gradings_to_do attribute)

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,5 +1,5 @@
 class Enrollment < ActiveRecord::Base
-  # Attributes: participant_id, course_id, status, sid, gradings_to_do
+  # Attributes: participant_id, course_id, status, sid
   belongs_to :participant, class_name: "User"
   belongs_to :course, class_name: "Course"
 
