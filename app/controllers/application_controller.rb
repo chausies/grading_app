655c255de1dec25c5b5c7c1ad5511ceec50ab249
chausies <mahatmaajaytripathi@gmail.com>
2014-08-20 04:32:05 -0700

    Updated header file so it's much much sexier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include ApplicationHelper
+	before_filter :assign_header_variables
+
+	def assign_header_variables
+		if signed_in? and current_user.courses.any? and current_user.enrollments.any?
+			@amount = (current_user.enrollments.map { |e| e.gradings_to_do.count }).sum
+		else
+			@amount = 0
+		end
+	end
 end
