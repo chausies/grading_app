66a6c9d40f92e4a8298ec3363cdbb4c1e5bf5a40
chausies <mahatmaajaytripathi@gmail.com>
2014-09-12 19:29:10 -0700

    Adding javascript to hopefully make dropdown's work on mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,12 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$('.dropdown-toggle').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
+});
